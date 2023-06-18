24a50cec72fff26dd91945047091fc49d43d2403
molindu162 <molinduachintha24@gmail.com>
2023-06-18 10:31:10 +0530

    Adding a practical example for Chain of Responsibility pattern

--- src/Builder/Meal.java
+++ src/Builder/Meal.java
@@ -1,6 +1,26 @@
 package Builder;
 
 public class Meal {
+    public String getMainDish() {
+        return mainDish;
+    }
+
+    public String getSideDish() {
+        return sideDish;
+    }
+
+    public String getDesert() {
+        return desert;
+    }
+
+    public String getSnack() {
+        return snack;
+    }
+
+    public String getDrink() {
+        return drink;
+    }
+
     private String mainDish;
     private String sideDish;
     private String desert;
@@ -8,6 +28,10 @@ public class Meal {
     private String drink;
 
     public Meal(String mainDish, String sideDish, String desert, String snack, String drink){
-
+        this.mainDish = mainDish;
+        this.sideDish = sideDish;
+        this.desert = desert;
+        this.snack = snack;
+        this.drink = drink;
     }
 }
---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import CustomerSupportTicketsManagement.*;
+
+public class Main {
+    public static void main (String[] args){
+        FeatureRequestHandler featureRequestHandler = new FeatureRequestHandler(null);
+        BillingHandler billingHandler = new BillingHandler(featureRequestHandler);
+        TechnicalSupportHandler technicalSupportHandler = new TechnicalSupportHandler(billingHandler);
+        ValidityChecker checker = new ValidityChecker(technicalSupportHandler);
+        checker.handle(new Ticket("1234", true, true));
+    }
+}
